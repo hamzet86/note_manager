3ca5f65b9b3abe75f9fe61016c2e9777ee090a46
hamzet86 <128816224+hamzet86@users.noreply.github.com>
2025-01-09 20:57:28 +0500

    Add files via upload

---
+++ reetings.py
@@ -0,0 +1,7 @@
+username,title,content,status,created_date,issue_date = input(), input(), input(), input(),input() ,input()
+print('Имя пользователя:' , username)
+print('Заголовок зметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', created_date)
+print('Дата истечения заметки:', issue_date)
